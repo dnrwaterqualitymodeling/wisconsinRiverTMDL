ac07d8d88ea747e05e06b81a72bdd1aa3b354271
Aaron Ruesch <asruesch@gmail.com>
2013-06-24 10:51:18 -0500

    changes to crop rotation scripts

--- cropRotations/rotationIdentification.R
+++ cropRotations/rotationIdentification.R
@@ -3,22 +3,22 @@ dominantCropsFigure = F
 countUniqueRotations = F
 veggiesLumped = T
 fiveYrRotBool = F
-crosstabFiles = c("D:/NASS/cropRotation/crosstab_dtrsq/wrb_cdl_2008.csv"
-                  , "D:/NASS/cropRotation/crosstab_dtrsq/wrb_cdl_2009.csv"
-                  , "D:/NASS/cropRotation/crosstab_dtrsq/wrb_cdl_2010.csv"
-                  , "D:/NASS/cropRotation/crosstab_dtrsq/wrb_cdl_2011.csv"
-                  , "D:/NASS/cropRotation/crosstab_dtrsq/wrb_cdl_2012.csv")
-rotationTableFile = "D:/NASS/cropRotation/rotationByQuarterSection_relumped.csv"
-domCropsFigureFile = "D:/NASS/cropRotation/dominantCrops_relumped.pdf"
-unqSortRotFile = "D:/NASS/cropRotation/uniquefiveYrRotations_relumped.csv"
-rotationFrequencyFile = "D:/NASS/cropRotation/rotationFrequencies_relumped.csv"
-legendFile = "D:/NASS/cropRotation/cdlAttributeTable.csv"
+crosstabFiles = c("C:/Users/benekt/Desktop/WRB_data/R_Code/crosstab_dtrsq/wrb_cdl_2008.csv"
+                  , "C:/Users/benekt/Desktop/WRB_data/R_Code/crosstab_dtrsq/wrb_cdl_2009.csv"
+                  , "C:/Users/benekt/Desktop/WRB_data/R_Code/crosstab_dtrsq/wrb_cdl_2010.csv"
+                  , "C:/Users/benekt/Desktop/WRB_data/R_Code/crosstab_dtrsq/wrb_cdl_2011.csv"
+                  , "C:/Users/benekt/Desktop/WRB_data/R_Code/crosstab_dtrsq/wrb_cdl_2012.csv")
+rotationTableFile = "C:/Users/benekt/Desktop/WRB_data/R_Code/lumpedrotations_sweetcorn_062413.csv"
+legendFile = "C:/Users/benekt/Desktop/WRB_data/R_Code/cdlAttributeTable.csv"
 legend = read.csv(legendFile)
 legend$CLASS_NAME = as.character(legend$CLASS_NAME)
 nonRotCropVals = c(0, 63:180, 182:203)
-referenceCrop = function (x, cropCodes, legend, collapse=T) {
+threshold = 5
+
+
+referenceCrop = function (x, cropCodes, legend, collapse=T, threshold) {
     if (length(which(x == max(x,na.rm=T))) > 1) {
-        if (max(x,na.rm=T) > 5) {
+        if (max(x,na.rm=T) > threshold) {
             cropCode = cropCodes[which(x == max(x,na.rm=T))]
             crop = legend$CLASS_NAME[legend$VALUE %in% cropCode]
             if (collapse) {
@@ -28,51 +28,49 @@ referenceCrop = function (x, cropCodes, legend, collapse=T) {
             crop = NA
         }
     } else {
-        cropCode = cropCodes[which(x == max(x,na.rm=T))]
-        crop = legend$CLASS_NAME[legend$VALUE == cropCode]
+        if (max(x,na.rm=T) > threshold) {
+            cropCode = cropCodes[which(x == max(x,na.rm=T))]
+            crop = legend$CLASS_NAME[legend$VALUE == cropCode]
+        }
     }
     return(crop=as.character(crop))
 }
+
+
+
 lumpCropTypes = function (crosstab,cropCodes,legend, veggiesLumped) {
-    corn = c(1,12,13)
-    alfalfa = 36
-    forage = c(37,58,62,181)
-    grain = c(4,21:25,27:30,205)
+    legend = legend[c("VALUE", "CLASS_NAME")]
+    corn = 1
+    sweetCorn = 12
+    alfalfa = c(4, 21, 22, 23, 24, 25, 27, 28, 29, 30, 36, 39, 58, 205)
+    pasture = c(37, 62, 181)
     soybeans = 5
     potatoes = 43
     dryBeans = 42
-    if (veggiesLumped) {
-        veggies = c(49,50,53,206,216)
-        mainCrops = c(corn,alfalfa,forage,grain,soybeans,potatoes,dryBeans,veggies)
-        other = cropCodes[-which(cropCodes %in% mainCrops)]
-        cropLbl = c("Corn", "Forage", "Grain", "Other", "Veggies")
-        crops = list(corn,forage,grain,other,veggies)
-    } else {
-        dryBeans = 42
-        potatoes = 43
-        onions = 49
-        cucumbers = 50
-        peas = 53
-        carrots = 206
-        peppers = 216
-        mainCrops = c(corn,forage,grain,soybeans,dryBeans,potatoes
-                      ,onions,cucumbers,peas,carrots,peppers)
-        other = cropCodes[-which(cropCodes %in% mainCrops)]
-        cropLbl = c("Corn", "Forage", "Grain", "Other")
-        list(corn,forage,grain,other)
-    }   
+    veggies = c(49,50,53,206,216)
+    mainCrops = c(corn,sweetCorn,alfalfa,pasture,soybeans,potatoes,dryBeans,veggies)
+    other = cropCodes[-which(cropCodes %in% mainCrops)]
+    cropLbl = c("Alfalfa", "Pasture", "Veggies", "Other")
+    crops = list(alfalfa, pasture, veggies, other)   
     i = 0
-    for (crop in crops) {
-        i = i + 1
-        cropCols = which(cropCodes %in% crop)
-        cropCol = cropCols[1]
-        legend$CLASS_NAME[cropCodes[cropCol] + 1] = cropLbl[i]
-        crosstab[,cropCol] = rowSums(crosstab[,cropCols], na.rm=T)
-        crosstab = crosstab[,-cropCols[-1]]
-        cropCodes = cropCodes[-cropCols[-1]]
-    }
+	for (crop in crops) {
+		i = i + 1
+		cropCols = which(cropCodes %in% crop)
+		cropCol = cropCols[1]
+		legend$CLASS_NAME[cropCodes[cropCol] + 1] = cropLbl[i]
+		if (length(cropCols) > 1) {
+			crosstab[,cropCol] = rowSums(crosstab[,cropCols], na.rm=T)
+			crosstab = crosstab[,-cropCols[-1]]
+			cropCodes = cropCodes[-cropCols[-1]]
+		} else if (length(cropCols) == 1) {
+			crosstab[,cropCol] = crosstab[,cropCols]
+		}
+	}
     return(list(crosstab=crosstab,cropCodes=cropCodes,legend=legend))
 }
+
+
+
 ##  Output a table of crop rotations
 domCropsAllYrs = NULL
 if (writeDomCropTable) {
@@ -95,21 +93,15 @@ if (writeDomCropTable) {
                          , referenceCrop
                          , cropCodes=lumped$cropCodes
                          , legend=lumped$legend
-                         , collapse=T)
+                         , collapse=T
+                         , threshold=threshold)
         domCrops = unlist(domCrops)
         print(length(domCrops))
         domCropsAllYrs = cbind(domCropsAllYrs, domCrops)
     }
-    if (veggiesLumped) {
-        mainCrops = c("Corn","Alfalfa","Forage","Grain","Soybeans","Potatoes"
-                      ,"Dry Beans", "Veggies", "Other")
-        mainAbbr = c("Co","Al","Fo","Gr","So","Po","Db","Vg","Ot")
-    } else {
-        mainCrops = c("Corn","Forage","Grain","Soybeans","Dry Beans"
-                      , "Potatoes", "Onions", "Cucumbers", "Peas"
-                      , "Carrots", "Peppers", "Other")
-        mainAbbr = c("Co","Fo","Gr","So","Db","Po","On","Cu","Pe","Ca","Pp","Ot")
-    }
+    mainCrops = c("Corn","Sweet Corn","Alfalfa", "Pasture", "Soybeans","Potatoes"
+                  ,"Dry Beans", "Veggies", "Other")
+    mainAbbr = c("Co","Sw","Al","Pa","So","Po","Db","Vg","Ot")
     i = 0
     for (crop in mainCrops) {
         i = i + 1
@@ -117,103 +109,10 @@ if (writeDomCropTable) {
     }
     domCropsAllYrs = cbind(crosstabMeta, domCropsAllYrs)
     names(domCropsAllYrs) = c("ID", "cellCount", "naCells", paste("yr", 2008:2012, sep=""))
-    if (fiveYrRotBool) {
-        rotStr = apply(domCropsAllYrs[paste("yr", 2008:2012, sep="")]
-                       , 1, function (x) {paste(x, collapse="-")})
-        pat = "^[A-Z][a-z]-[A-Z][a-z]-[A-Z][a-z]-[A-Z][a-z]-[A-Z][a-z]$"
-        fiveYrRot = grep(pat,rotStr)
-        domCropsAllYrs = domCropsAllYrs[fiveYrRot,]
-    }
     write.csv(domCropsAllYrs, file=rotationTableFile, row.names=F)
 }
-##  Output a frequency table of crops per year
-if (dominantCropsFigure) {
-    pdf(file=domCropsFigureFile,width=8.5,height=11)
-    par(mfrow=c(3,2))
-    yrs = 2008:2012
-    i=0
-    for (crosstabFile in crosstabFiles) {
-        i = i + 1
-        print(crosstabFile)
-        crosstab = read.csv(crosstabFile)
-        crosstabMeta = crosstab[,c(1,2,ncol(crosstab))]
-        crosstab = crosstab[,-c(1,2,ncol(crosstab))]
-        cropCodes = gsub("W_[0-9]*_[0-9]*m_cdls_", "", names(crosstab))
-        if (length(which(cropCodes == "NoData"))) {
-            cropCodes[which(cropCodes == "NoData")] = "255"
-        }
-        cropCodes = as.integer(cropCodes)
-        crosstab = as.matrix(crosstab)  
-        cropCols = which(!(cropCodes %in% nonRotCropVals))
-        crosstab = crosstab[,cropCols]
-        cropCodes = cropCodes[cropCols]
-        lumped = lumpCropTypes(crosstab,cropCodes,legend,veggiesLumped=veggiesLumped)
-        domCrops = apply(lumped$crosstab, 1
-                         , referenceCrop
-                         , cropCodes=lumped$cropCodes
-                         , legend=lumped$legend
-                         , collapse=F)
-        domCrops = unlist(domCrops)
-        freq = sort(table(domCrops), decreasing=TRUE)
-        barplot(freq, las=3, col=rainbow(length(freq)), main=paste("Year", yrs[i]))
-    }
-    dev.off()
-}
-# Count the number of unique rotations
-domCropsAllYrs = NULL
-if (countUniqueRotations) {
-    for (crosstabFile in crosstabFiles) {
-        print(crosstabFile)
-        crosstab = read.csv(crosstabFile)
-        crosstabMeta = crosstab[,c(1,2,ncol(crosstab))]
-        crosstab = crosstab[,-c(1,2,ncol(crosstab))]
-        cropCodes = gsub("W_[0-9]*_[0-9]*m_cdls_", "", names(crosstab))
-        if (length(which(cropCodes == "NoData"))) {
-            cropCodes[which(cropCodes == "NoData")] = "255"
-        }
-        cropCodes = as.integer(cropCodes)
-        crosstab = as.matrix(crosstab)  
-        cropCols = which(!(cropCodes %in% nonRotCropVals))
-        crosstab = crosstab[,cropCols]
-        cropCodes = cropCodes[cropCols]
-        lumped = lumpCropTypes(crosstab,cropCodes,legend,veggiesLumped=veggiesLumped)
-        domCrops = apply(lumped$crosstab, 1
-                         , referenceCrop
-                         , cropCodes=lumped$cropCodes
-                         , legend=lumped$legend
-                         , collapse=T)
-        domCrops = unlist(domCrops)
-        print(length(domCrops))
-        domCropsAllYrs = cbind(domCropsAllYrs, domCrops)
-    }
-    if (veggiesLumped) {
-        mainCrops = c("Corn","Alfalfa","Forage","Grain","Soybeans","Potatoes"
-                      ,"Dry Beans","Veggies", "Other")
-        mainAbbr = c("Co","Al","Fo","Gr","So","Po","Db","Vg","Ot")
-    } else {
-        mainCrops = c("Corn","Forage","Grain","Soybeans","Dry Beans"
-                      , "Potatoes", "Onions", "Cucumbers", "Peas"
-                      , "Carrots", "Peppers", "Other")
-        mainAbbr = c("Co","Fo","Gr","So","Db","Po","On","Cu","Pe","Ca","Pp","Ot")
-    }
-    i = 0
-    for (crop in mainCrops) {
-        i = i + 1
-        domCropsAllYrs[which(domCropsAllYrs == crop)] = mainAbbr[i]
-    }
-    domCropsAllYrs = cbind(crosstabMeta, domCropsAllYrs)
-    names(domCropsAllYrs) = c("ID", "cellCount", "naCells", paste("yr", 2008:2012, sep=""))
-    sortedRotations = apply(domCropsAllYrs[paste("yr", 2008:2012, sep="")]
-                            , 1, function(x) {paste(sort(x), collapse="-")})
-    rotFreq = sort(table(sortedRotations), decreasing=T)
-    write.csv(rotFreq, file=rotationFrequencyFile, row.names=T)
-    pat = "^[A-Z][a-z]-[A-Z][a-z]-[A-Z][a-z]-[A-Z][a-z]-[A-Z][a-z]$"
-    unqSortRot = sort(unique(sortedRotations))    
-    fiveYrRot = grep(pat,unqSortRot)    
-    unqSortRot = unqSortRot[fiveYrRot]
-    unqSortRot = unqSortRot[!is.na(unqSortRot)]
-    write(unqSortRot, file=unqSortRotFile)
-}
+
+
 
 
 
