96aefb9265b59cf196e322dcf12a4a7ac4a46624
Aaron Ruesch <asruesch@gmail.com>
2014-08-12 10:44:04 -0500

    code for calculating pond geometry

--- ponds/calculatePondParameters.r
+++ ponds/calculatePondParameters.r
@@ -1,14 +1,23 @@
-library(xlsx)
+# library(xlsx)
 library(rgdal)
+library(rgeos)
+library(raster)
 
 wd = "T:/Projects/Wisconsin_River/GIS_Datasets"
 setwd(wd)
 
-lake_volume_data = read.xlsx("ponds/WRT_07_19_13.xlsx", sheetName="data")
+# lake_volume_data = read.xlsx("ponds/WRT_07_19_13.xlsx", sheetName="data")
+lake_volume_data = read.csv("ponds/WRT_07_19_13.csv")
 lake_volume_data$Volume..acre.ft.[lake_volume_data$Volume..acre.ft. == 0] = NA
 lake_volume_data$Max.Depth..ft.[lake_volume_data$Max.Depth..ft.] = NA
 wb = readOGR("ponds/waterbodies.gdb", "lake_pond")
-watersheds = readOGR("Watersheds/HUC_Subwatersheds", "WRB_HUC16")
+watersheds = readOGR("Watersheds/HUC_Subwatersheds", "WRB_HUC16_WTM")
+subbasins = readOGR("T:/Projects/Wisconsin_River/Model_Inputs/SWAT_Inputs/hydro",
+    "aggregate_subsheds_V6")
+dem = raster("DEM/raw_prj_10_m.img")
+demFill = raster("DEM/wrb_fill")
+proj4string(watersheds) = proj4string(wb)
+proj4string(subbasins) = proj4string(wb)
 
 wb_vol = merge(wb@data,
     lake_volume_data,
@@ -34,4 +43,66 @@ wb_vol$Area..acres.[area_ind] = wb_vol$SHAPE_Area[area_ind] / 4046.86
 wb_vol$Volume..acre.ft.[area_ind] = exp(predict(area_model, wb_vol[area_ind,]))
 wb_vol$Volume..acre.ft.[area_depth_ind] = exp(predict(area_depth_model, wb_vol[area_depth_ind,]))
 
+wb@data = wb_vol
 
+wb_ll = wb_vol[which(wb_vol$LANDLOCK_CODE == 1),]
+watersheds_ll = subset(watersheds, CATCHID %in% wb_ll$HYDROID)
+
+catchids_ll = watersheds_ll@data$CATCHID
+end = F
+while (!end) {
+    count = length(catchids_ll)
+    upstream_catchids = watersheds_ll@data$TOCATCHID %in% catchids_ll
+    catchids_ll = unique(c(catchids_ll, upstream_catchids))
+    if (length(catchids_ll == count)) {
+        end = T
+    }
+}
+watersheds_ll = subset(watersheds, CATCHID %in% catchids_ll)
+
+geometry_table = data.frame()
+for (s in subbasins@data$Subbasin) {
+#     if (s == 3) {break}
+    print(paste("Subbasin number", s))
+    subbasin = subset(subbasins, Subbasin == s)
+    # If there are no land-locked watersheds in the subbasin, move onto the next iteration
+    if (any(gContains(subbasin, watersheds_ll, byid=T)[,1]) == F) {next}
+    contained_watersheds = subset(watersheds_ll, gContains(subbasin, watersheds_ll, byid=T)[,1])
+    dissolve_watersheds = gUnionCascaded(contained_watersheds)
+    # If there are no land-locked WATERBODIES in the landlocked watersheds, move onto the next iteration
+    if (any(gContains(dissolve_watersheds, wb, byid=T)[,1]) == F) {next}
+    contained_ponds = subset(wb, gContains(dissolve_watersheds, wb, byid=T)[,1])
+    e = alignExtent(dissolve_watersheds, dem)
+    mask_dem = mask(crop(dem, e), dissolve_watersheds)
+    mask_dem_fill = mask(crop(demFill, e), dissolve_watersheds)
+    fill_height = mask_dem_fill - mask_dem
+    clumps = clump(fill_height > 0, directions=8)
+    totalArea = 0
+    totalVolumeChange = 0
+    for (catchid in contained_watersheds@data$CATCHID) {
+        print(paste("assessing emergency geometry within subbasin", catchid))
+        w = subset(contained_watersheds, CATCHID==catchid)
+        e_w = alignExtent(w, dem)
+        mask_clumps = mask(crop(clumps, e_w), w)
+        if (all(is.na(getValues(mask_clumps)))) { next }
+        clumps_poly = rasterToPolygons(mask_clumps, dissolve=T)
+        maxArea = max(gArea(clumps_poly, byid=T))
+        totalArea = totalArea + maxArea # square meters
+        maxSizeId = clumps_poly@data$clumps[which(gArea(clumps_poly, byid=T) == maxArea)]
+        mask_fill_height = mask(crop(fill_height, e_w), w)
+        heights = mask_fill_height[mask_clumps == maxSizeId]
+        d_vol = sum(heights * 100, na.rm=T)
+        totalVolumeChange = totalVolumeChange + d_vol # cubic meters
+    }
+    row = data.frame(
+        subbasin = subbasin@data$Subbasin,
+        PND_FR = gArea(contained_watersheds) / gArea(subbasin),
+        PND_PSA = gArea(contained_ponds) / 10e4,
+        PND_PVOL = sum(contained_ponds$Volume..acre.ft., na.rm=T) * 8.107132,
+        PND_ESA = totalArea / 10e4,
+        PND_EVOL = (sum(contained_ponds$Volume..acre.ft., na.rm=T) * 8.107132) + 
+            (totalVolumeChange / 10e4)
+    )
+    geometry_table = rbind(geometry_table, row)
+}
+write.csv(geometry_table, file="ponds/pond_geometry.csv", row.names=F)
---
+++ ponds/import_pond_data_to_swat_database.r
@@ -0,0 +1,33 @@
+library(RODBC)
+
+# CHANGE THESE ###########
+# SWAT project
+projectDir = "C:/SWAT/Reservoirs_2"
+pond_geometry_file = "T:/Projects/Wisconsin_River/GIS_Datasets/ponds/pond_geometry.csv"
+
+pond_geometry = read.csv(pond_geometry_file)
+names(pond_geometry)[1] = "SUBBASIN"
+
+inDb = paste(projectDir, "Reservoirs_2.mdb", sep="/")
+con = odbcConnectAccess(inDb)
+
+pndData = sqlQuery(con, "SELECT * FROM pnd")
+
+for (row in 1:nrow(pond_geometry)) {
+	query = paste(
+		"UPDATE pnd\n",
+		"SET PND_FR = ", pond_geometry$PND_FR[row], ",",
+		"PND_PSA = ", pond_geometry$PND_PSA[row], ",",
+		"PND_PVOL = ", pond_geometry$PND_PVOL[row], ",",
+		"PND_ESA = ", pond_geometry$PND_ESA[row], ",",
+		"PND_EVOL = ", pond_geometry$PND_EVOL[row], ",",
+		"PND_NDTARG = ", pond_geometry$NDTARG[row], "\n",
+		"WHERE SUBBASIN = ", pond_geometry$SUBBASIN[row], ";",
+		sep = ""
+	)
+}
+
+pndData = 
+close(con)
+
+
