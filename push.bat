f282f263a2b4588b9029bb20c8ecfcf18f4a8f27
Dave-Evans <evans.dave.michael@gmail.com>
2014-11-18 17:16:47 -0600

    end of the day commit

--- push.bat
+++
@@ -1,3 +0,0 @@
-C:\Users\evansdm\AppData\Local\Programs\Git\bin\git.exe add . --all
-C:\Users\evansdm\AppData\Local\Programs\Git\bin\git.exe commit 
-C:\Users\evansdm\AppData\Local\Programs\Git\bin\git.exe push origin master
