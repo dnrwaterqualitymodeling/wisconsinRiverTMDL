794d124a111fa1b57606413a1bc9911e3017a846
Dave-Evans <evans.dave.michael@gmail.com>
2014-11-18 15:59:58 -0600

    test push. Hi

--- push.bat
+++ push.bat
@@ -1,4 +1,3 @@
 C:\Users\evansdm\AppData\Local\Programs\Git\bin\git.exe add . --all
 C:\Users\evansdm\AppData\Local\Programs\Git\bin\git.exe commit 
-C:\Users\evansdm\AppData\Local\Programs\Git\bin\git.exe push
-C:\Users\evansdm\AppData\Local\Programs\Git\bin\git.exe
+C:\Users\evansdm\AppData\Local\Programs\Git\bin\git.exe push origin master
