d347c21aa44f740f7e3687f2eb51badaed8e6556
Dave-Evans <evans.dave.michael@gmail.com>
2014-12-16 18:37:22 -0600

    updates to figures and some tables

---
+++ figures/function_proper_legend.r
@@ -0,0 +1,22 @@
+#### for proper legend #####
+# returns proper legend text for intervals created from classInterval-color object, the output of findColours
+properLegend <- function(prop_colr, sig_figs = 0){
+    legTxt <- attr(attr(prop_colr,'table'),'dimnames')[[1]]
+    newTxt <- NULL
+    for (rw in 1:length(legTxt)){
+        r <- legTxt[rw]
+        splt <- strsplit(r, split=',')[[1]]
+        opn <- as.numeric(strsplit(splt[1],'[',fixed=T)[[1]][2])
+        if (rw == length(legTxt)){
+            cls <- as.numeric(strsplit(splt[2],']')[[1]][1])
+        } else {
+            cls <- as.numeric(strsplit(splt[2],')')[[1]][1])
+        }
+#         print(paste(opn,cls))
+        opn <- round(opn, sig_figs)
+        cls <- round(cls, sig_figs)
+        nw <- paste('[',opn,',',cls,')',sep='')
+        newTxt <- c(newTxt, nw)
+    }
+    return(newTxt)
+}
