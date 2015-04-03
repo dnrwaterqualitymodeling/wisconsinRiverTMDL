80e98adad1d3c5037d2d37f71c85d625b62eecd2
Guy Hydrick <hydrickg@gmail.com>
2015-04-03 16:21:41 -0500

    finalized SLAMM to SWAT script

--- urban/slamm_to_master_excel_Final.py
+++ urban/slamm_to_master_excel_Final.py
@@ -4,9 +4,9 @@
 #-------------------------------------#
 
 import arcpy, os, subprocess
-from datetime import date, timedelta
 
 arcpy.env.overwriteOutput = True
+
 
 #declare variables -----------------
 
@@ -24,7 +24,7 @@ arcpy.env.workspace = TempGDB
 
 #the folder which all SLAMM output .csv files should be in
 SLAMM_folder = "T:/Projects/Wisconsin_River/Model_Inputs/WinSLAMM_Inputs/WinSLAMM"
-#SLAMM_folder = "C:/Users/hydrig/Documents/Projects/WinSLAMM"
+
 #folder for cleaned up CSV files
 cleanCSV = "C:/Users/hydrig/Documents/Projects/wisconsinRiverTMDL/urban/clean_csv.r"
 #Location of R -- You’ll want to check your version here, you’re maybe 3.3.1…
@@ -69,61 +69,15 @@ precipFOIs_clean = [
 "Rain Start Date",
 ]
 
-# masterFOIs = [
-# 'Date',
-# 'Subbasin',
-# 'Sed_load',
-# 'Min_P_Load',
-# 'Org_P_Load',
-# 'Muni'
-# ]
-
 
 #define functions--------------------
 
-#Dave created an R script to work around the read-only problem that was sporadically messing up this process in the next function; call that
+
+#Dave created an R script to work around the read-only problem that was sporadically messing up this process in arcpy; call this instead
 def cleanCSVs():
 	cmd = rscript + " " + cleanCSV
 	p = subprocess.Popen(cmd)
 	p.wait()
-
-# # generate list of SLAMM .csv files to be loaded
-# # Ann said the .csv files were the authoritative outputs
-# # cleaned up file naming convention (CL, SA, SL) by hand (in the raw files from Ann)
-# def getSlammOutputs():
-	# # arcpy.env.workspace = SLAMM_folder
-	# arcpy.env.workspace = SLAMM_folder + "\\cleanedCsvs"
-	# # wksps = arcpy.ListWorkspaces()
-	# # for w in wksps:
-		# # arcpy.env.workspace = w
-	# csv_files = arcpy.ListFiles("*.csv")
-		# # if csv_files == "None":
-			# # continue
-	# for csv in csv_files:
-		# print csv
-			# clean up .csv names and convert to GDB tables
-			# # TableName = os.path.splitext(csv)[0]
-			# # TableName = TableName.replace('-Output', '')
-			# # TableName = TableName.rstrip()
-		# arcpy.TableToTable_conversion(csv, TempGDB, csv.replace(".csv", ""))		#convert to GDB tables so I can use cursors later - cursors on .csv files give strange results
-			
-			# # date field is not really a date format field, so add one
-		# # arcpy.AddField_management(TempGDB + '\\' + csv, "Join_Date", "DATE")						#is this used anymore, with no joins? Can calc later?
-			
-			# clean summaries off bottom of tables
-			# # getRows = arcpy.GetCount_management(TempGDB + '\\' + TableName)
-			# # numRows = int(getRows.getOutput(0))
-			# # with arcpy.da.UpdateCursor(TempGDB + '\\' + TableName, ["Rain_Start_Date", "Join_Date"]) as cursor:
-				# # for r, row in enumerate(cursor):
-					# # if numRows - r <= 13:			#summaries are last 13 rows	
-						# # cursor.deleteRow()
-					# # else:
-						# # row[1] = row[0]				#calc date field
-						# # cursor.updateRow(row)
-			# # del cursor
-			
-	# # reset workspace
-	# arcpy.env.workspace = TempGDB
 
 #create sewer/reach sheds by unique muni, subbasin, soil type, and add best precip file
 def createReachsheds():
@@ -174,7 +128,7 @@ def calcDailyReachLoads():
 			if row[1] in soil_key:
 				pre_file = row[2] + '_' + soil_key[row[1]]
 				print pre_file
-				acreage = row[0] * 0.0247105381 #area in sqm converted to 100 acres
+				acreage = row[0] * 0.0247105381 	#area in sqm converted to 100 acres
 				subbasin = row[3]
 				muni = row[4]
 				all_loads = []
@@ -183,25 +137,20 @@ def calcDailyReachLoads():
 				csv_clean = SLAMM_folder + "/cleanedCsvs/" + pre_file + ".csv"
 				with arcpy.da.SearchCursor(csv_clean, precipFOIs_clean) as cursor2:
 					for row2 in cursor2:
-						#convert units to tons and kg, respectively
-						#try:
 						d_load = (
 							row2[4],
 							subbasin,
-							noNull(row2[0]) * acreage * 0.0283168,
-							noNull(row2[1]) * acreage * 0.00045359237,
-							noNull(row2[3]) * acreage * 0.453592,
-							noNull(row2[2]) * acreage * 0.453592,
+							noNull(row2[0]) * acreage * 0.0283168,		#cubic feet to cubic meters
+							noNull(row2[1]) * acreage * 0.00045359237,	#pounds to metric tonnes
+							noNull(row2[3]) * acreage * 0.453592,		#pounds to kilograms
+							noNull(row2[2]) * acreage * 0.453592,		#pounds to kilograms
 							muni
 							)
 						all_loads.append(d_load)
-						# print d_load
-						#except:
-						#	raise Exception(row2)
+
 				del cursor2
 
-				
-				#write headers
+				#write headers and output to tab delimited txt file
 				headers = ["date", "subbasin", "flow_m3", "TSS_tons", "P_filt_kg", "P_part_kg", "muni"]
 				if i == 1:
 					f.write("\t".join(headers) + "\n")
@@ -210,37 +159,15 @@ def calcDailyReachLoads():
 					out_row[0] = out_row[0].strftime("%Y-%m-%d")
 					f.write("\t".join(map(str, out_row)) + "\n")
 				
-				#write precip events to master loads table
-				# with arcpy.da.InsertCursor("MasterLoadsA", masterFOIs) as cursor3:
-					# for d in all_loads:
-						# cursor3.insertRow(d)
-	del cursor
-	f.close()
-	#aggregate by data, subbasin, and muni
-	# arcpy.Statistics_analysis("MasterLoadsA", "FinalOutput", [["Min_P_Load","SUM"],["Org_P_Load","SUM"],["Sed_load","SUM"]], ["Date","Subbasin","Muni"])
-	
-	
-def outputToTxt(outFile):
-	filepath = "C:/Users/hydrig/Desktop/"
-	f = open(filepath + outFile, "w")
-	#write headers
-	headers = ["OID", "Date", "Subbasin", "Muni", "Min_P", "Org_P", "Total_Sed"]
-	f.write("\t".join(headers) + "\n")
-	with arcpy.da.SearchCursor("FinalOutput", ["OBJECTID", "Date", "Subbasin", "Muni", "SUM_Min_P_Load", "SUM_Org_P_Load", "SUM_Sed_load"]) as cursor:
-		for row in cursor:
-			f.write("\t".join(map(str, row)) + "\n")
 	del cursor
 	f.close()
 	
-
+	
 #execute script-----------------------------
 
-cleanCSVs()
 
-getSlammOutputs()
+cleanCSVs()
 
 createReachsheds()
 
 calcDailyReachLoads()
-
-outputToTxt('S2S_OutputA.txt')
