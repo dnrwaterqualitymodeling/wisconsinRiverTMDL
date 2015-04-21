12e9fedd3e389d2ae2133c202065e8a5fd83686a
Dave-Evans <evans.dave.michael@gmail.com>
2015-04-21 15:09:34 -0500

    completed script for creating plots in to assess point source input data

--- pointSources/assess_point_source_inputs.r
+++ pointSources/assess_point_source_inputs.r
@@ -8,6 +8,33 @@ lu_file = "T:/Projects/Wisconsin_River/Model_Documents/Point_Source_Info/DRAFT W
 out_dir = "T:/Projects/Wisconsin_River/Model_Inputs/SWAT_Inputs/point_sources"
 log_file = "T:/Projects/Wisconsin_River/Model_Documents/Point_Source_Info/DRAFT WASTEWATER DATASET FILES/issues.log"
 
+arcswat_hdr = c(
+	"DATE",
+	"Floday",
+	"Sedday",
+	"Orgnday",
+	"Orgpday",
+	"No3day",
+	"Nh3day",
+	"No2day",
+	"Minpday",
+	"Cbodday",
+	"Disoxday",
+	"Chladay",
+	"Solpstday",
+	"Srbpstday",
+	"Bactpday",
+	"Bactlpday",
+	"Cmtl1day",
+	"Cmtl2day",
+	"Cmtl3day")
+
+vars = list(
+	c("Floday", "mean_flow", "cubic meters per second"),
+	c("Sedday","sed_load", "Mg/day"),
+	c("Minpday", "p_min_load", "kg/day"),
+	c("Orgpday", "p_org_load", "kg/day"))
+
 # read in point source monthly from tab delimited file
 of_data = read.delim(of_file)
 muni_data = read.delim(muni_file)
@@ -23,10 +50,12 @@ dates = data.frame(
 	MON = as.integer(format(dates, "%m")),
 	DAY = as.integer(format(dates, "%j"))
 )
-
+pdf("Pt_src_assessment.pdf", width=11,height=8)
 output_holder = data.frame()
 # for (sb_id in unique(lu_table$SUBBASIN)) {
 for (sb_id in 1:337) {
+	# file_pdf = paste("pt_srcs_subbasin_", sb_id, ".pdf", sep="")
+	# pdf(file_pdf, width=11,height=8)
 	# sample_pt in sample_pts) {
 	print(sb_id)
 	ofs = subset(lu_table, SUBBASIN == sb_id)$SAMPLE_PT
@@ -63,23 +92,20 @@ for (sb_id in 1:337) {
 			sub_of_data = rbind(sub_of_data, out_day)
 		}
 		aggregated_of = aggregate(
-			cbind(mean_flow, sed_load, p_load) ~ YEAR + DAY, 
+			cbind(mean_flow, sed_load, p_load) ~ DATE, 
 			data=sub_of_data, 
 			FUN=sum,
 			na.rm=T)
-		aggregated_of = merge(dates, aggregated_of, all.x=T, all.y=F)
+		# aggregated_of = merge(dates, aggregated_of, all.x=T, all.y=F)
 		# Seperate P into 58% organic and 42% mineral
 		aggregated_of$p_org_load = aggregated_of$p_load * 0.58
 		aggregated_of$p_min_load = aggregated_of$p_load * 0.42
-		aggregated_of = aggregated_of[,-6]
-		aggregated_of = aggregated_of[order(aggregated_of$DATE),]
-		if (sum(aggregated_of[,4:7], na.rm=T) == 0) {
-			ofs_issues = paste(ofs, collapse=",")
-			write(paste(ofs_issues, "SAMPLE_PTs have all zero data"), log_file, append=T)
-		}
+		# aggregated_of = aggregated_of[,-6]
+		# aggregated_of = aggregated_of[order(aggregated_of$DATE),]
+
 	} else {
 		of_bool = F
-	}	
+	}
 	########################################
 	# Cacluate urban loads by subbasin
 	muni_sub = subset(muni_data, subbasin == sb_id)
@@ -106,55 +132,87 @@ for (sb_id in 1:337) {
 			all.x=T,
 			all.y=F
 		)
-		muni_sub_agg = muni_sub_agg[order(muni_sub_agg$date),]
+		muni_sub_agg = muni_sub_agg[order(muni_sub_agg$DATE),]
 	} else {
 		muni_bool = F
 	}
 	
 	if (!muni_bool & !of_bool) {
+		print("	Neither municipal nor wastewater sources.")
 		next
-	} else if (muni_bool & !of_bool) {
-		all_point_sources = muni_sub_agg
-	} else if (!muni_bool & of_bool) {
-		all_point_sources = aggregated_of
-	} else if (muni_bool & of_bool) {
-		all_point_sources = muni_sub_agg
-		COIs = c("mean_flow", "sed_load", "p_org_load", "p_min_load")
-		muni_sub_agg = muni_sub_agg[order(muni_sub_agg$YEAR, muni_sub_agg$DAY), ]
-		aggregated_of = aggregated_of[order(aggregated_of$YEAR, aggregated_of$DAY), ]
-		all_point_sources[COIs] = muni_sub_agg[COIs] + aggregated_of[COIs]
 	}
-	# Append the dailies to a master data.frame with subbasin ID associated
-	all_point_sources$sb_id = sb_id
-	output_holder = rbind(output_holder, all_point_sources)
+	comb_muni_wstwtr = read.delim(
+		list.files(out_dir, full.names=T)[grepl(paste("_",sb_id,".txt",sep=''),list.files(out_dir, full.names=T))]
+	)
+
+	comb_muni_wstwtr = strsplit(comb_muni_wstwtr[,1], split=",")
+	nrws = length(comb_muni_wstwtr)
+	ncls = length(comb_muni_wstwtr[[1]])
+	comb_muni_wstwtr = unlist(comb_muni_wstwtr)
+	comb_muni_wstwtr = matrix(comb_muni_wstwtr, nrow=nrws, ncol=ncls, byrow=TRUE)
+	comb_muni_wstwtr = as.data.frame(comb_muni_wstwtr)
+	comb_muni_wstwtr[,2:ncls] = apply(
+		comb_muni_wstwtr[,2:ncls],
+		MARGIN=2,
+		FUN=as.numeric
+	)
+	comb_muni_wstwtr[,1] = as.Date(comb_muni_wstwtr[,1], format="%m/%d/%Y")
+	names(comb_muni_wstwtr) = arcswat_hdr
+	comb_muni_wstwtr = comb_muni_wstwtr[4384:nrws,]
+	comb_muni_wstwtr[,2:ncls][comb_muni_wstwtr[,2:ncls] == 0] = NA
+
+	for (var in vars){
+		print(var)
+		lgnd = c("Total Pt Src Dischg")
+		clrs = c("#BEBEBE99")
+		if (sum(is.na(comb_muni_wstwtr[,var[1]])) == nrow(comb_muni_wstwtr)){
+			next
+		}
+		plot(comb_muni_wstwtr[,var[1]] ~ DATE, 
+			data=comb_muni_wstwtr,
+			# type='l',
+			col="#BEBEBE99",
+			lwd=4,
+			ylab=var[3],
+			main=var[2],
+			ylim=c(0, max(comb_muni_wstwtr[,var[1]],na.rm=T)),
+			sub=paste("Subbasin", sb_id))
+		
+		if (muni_bool) {
+			points(muni_sub_agg[,var[2]] ~ DATE,
+				data=muni_sub_agg,
+				col="#ff000080",
+				pch=21)
+			lgnd = c(lgnd, "Municipal Src")
+			clrs = c(clrs, "#ff000080")
+			# all_point_sources = muni_sub_agg
+		}
+		if (of_bool) {
+			lines(aggregated_of[,var[2]] ~ DATE,
+				data=aggregated_of,
+				col="#0000ff80")
+			lgnd = c(lgnd, "WasteWater Src")
+			clrs = c(clrs, "#0000ff80")
+		}
+		legend("topright",
+			legend=lgnd,
+			fill=clrs)
+	}
+
 }
-output_holder[is.na(output_holder)] = 0
+dev.off()
+	# Append the dailies to a master data.frame with subbasin ID associated
+	# all_point_sources$sb_id = sb_id
+	# output_holder = rbind(output_holder, all_point_sources)
 
-arcswat_hdr = c(
-	"DATE",
-	"Floday",
-	"Sedday",
-	"Orgnday",
-	"Orgpday",
-	"No3day",
-	"Nh3day",
-	"No2day",
-	"Minpday",
-	"Cbodday",
-	"Disoxday",
-	"Chladay",
-	"Solpstday",
-	"Srbpstday",
-	"Bactpday",
-	"Bactlpday",
-	"Cmtl1day",
-	"Cmtl2day",
-	"Cmtl3day")
+# output_holder[is.na(output_holder)] = 0
 
-out_tbl = matrix("0.000", nrow=nrow(dates), ncol=length(arcswat_hdr))
-out_tbl = data.frame(out_tbl)
-names(out_tbl) = arcswat_hdr
-dates = seq(as.Date("1990-01-01"), as.Date("2013-12-31"), by="1 day")
-out_tbl$DATE = format(dates, "%m/%d/%Y")
-out_tbl$DATE = gsub("^0", "", out_tbl$DATE)
-out_tbl$DATE = gsub("(/0)", "/", out_tbl$DATE)
+
+
+# out_tbl = matrix("0.000", nrow=nrow(dates), ncol=length(arcswat_hdr))
+# out_tbl = data.frame(out_tbl)
+# names(out_tbl) = arcswat_hdr
+# dates = seq(as.Date("1990-01-01"), as.Date("2013-12-31"), by="1 day")
+# out_tbl$DATE = format(dates, "%m/%d/%Y")
+# out_tbl$DATE = gsub("^0", "", out_tbl$DATE)
+# out_tbl$DATE = gsub("(/0)", "/", out_tbl$DATE)
