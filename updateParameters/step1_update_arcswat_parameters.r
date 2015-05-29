a7dd9a5edcb42186f3fffe85c82b5f9a7e0e0705
hydrickg <hydrickg@gmail.com>
2015-05-29 12:55:28 -0500

    fixed conflicts

--- updateParameters/step1_update_arcswat_parameters.r
+++ updateParameters/step1_update_arcswat_parameters.r
@@ -13,8 +13,8 @@ library(rgdal)
 options(stringsAsFactors=F)
 options(warn=1)
 # CHANGE THESE ACCORDING TO SWAT PROJECT
-projectDir = "C:/Users/ruesca/Desktop/WRB"
-#projectDir = "F:/WRB"
+#projectDir = "C:/Users/ruesca/Desktop/WRB"
+projectDir = "E:/WRB"
 mean_slope_file = "T:/Projects/Wisconsin_River/Model_Inputs/SWAT_Inputs/slope/subbasin_landuse_mean_slope.txt"
 
 #pond_geometry_file = "T:/Projects/Wisconsin_River/GIS_Datasets/ponds/pond_geometry.csv"
